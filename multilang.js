e701ae59b2066f14d615d1e34109c9d911688334
Kostiantyn Samusiev <djfoozyhook@gmail.com>
2025-01-10 17:56:32 +0200

    add new language
    add new language

--- multilang.js
+++ multilang.js
@@ -1,118 +1,101 @@
 var langs = {
     en: {
-        txt1: 'Women have asked us to not allow men that are seeking a "relationship".  <br><br> Do you agree to this request?',
         text1: 'This is NOT a dating site!',
         text2: 'You will simply choose a woman and ask to fuck!',
         text3: 'Please keep their identity a secret!',
         btn: 'I AGREE'
     },
     de: {
-        txt1: 'Frauen haben uns gebeten, keine Männer zuzulassen, die eine "Beziehung" suchen.  <br><br> Stimmen Sie dieser Bitte zu?',
         text1: 'Dies ist KEINE Dating-Seite!',
         text2: 'Sie wählen einfach eine Frau aus und fragen nach Sex!',
         text3: 'Bitte halten Sie ihre Identität geheim!',
         btn: 'ICH STIMME ZU'
     },
     fr: {
-        txt1: 'Les femmes nous ont demandé de ne pas autoriser les hommes qui recherchent une "relation".  <br><br> Acceptez-vous cette demande ?',
         text1: 'Ce n’est PAS un site de rencontres !',
         text2: 'Vous choisirez simplement une femme et lui proposerez de coucher ensemble !',
         text3: 'Veuillez garder leur identité secrète !',
         btn: 'JE SUIS D’ACCORD'
     },
     es: {
-        txt1: 'Las mujeres nos han pedido que no permitamos hombres que buscan una "relación".  <br><br> ¿Estás de acuerdo con esta solicitud?',
         text1: '¡Este NO es un sitio de citas!',
         text2: 'Simplemente elegirás a una mujer y le pedirás follar!',
         text3: '¡Por favor, mantén su identidad en secreto!',
         btn: 'ESTOY DE ACUERDO'
     },
     it: {
-        txt1: 'Le donne ci hanno chiesto di non permettere agli uomini che cercano una "relazione".  <br><br> Sei d’accordo con questa richiesta?',
         text1: 'Questo NON è un sito di incontri!',
         text2: 'Sceglierai semplicemente una donna e chiederai di fare sesso!',
         text3: 'Per favore, mantieni segreta la loro identità!',
         btn: 'SONO D’ACCORDO'
     },
     cs: {
-        txt1: 'Ženy nás požádaly, abychom neumožňovali mužům hledajícím "vztah".  <br><br> Souhlasíte s tímto požadavkem?',
         text1: 'Toto NENÍ seznamka!',
         text2: 'Jednoduše si vyberete ženu a požádáte ji o sex!',
         text3: 'Prosím, zachovejte jejich identitu v tajnosti!',
         btn: 'SOUHLASÍM'
     },
     da: {
-        txt1: 'Kvinder har bedt os om ikke at tillade mænd, der søger et "forhold".  <br><br> Er du enig i denne anmodning?',
         text1: 'Dette er IKKE en dating-side!',
         text2: 'Du vælger simpelthen en kvinde og beder om sex!',
         text3: 'Hold venligst deres identitet hemmelig!',
         btn: 'JEG ER ENIG'
     },
     nl: {
-        txt1: 'Vrouwen hebben ons gevraagd om geen mannen toe te laten die een "relatie" zoeken.  <br><br> Bent u het eens met dit verzoek?',
         text1: 'Dit is GEEN datingsite!',
         text2: 'Je kiest simpelweg een vrouw en vraagt om seks!',
         text3: 'Houd hun identiteit alstublieft geheim!',
         btn: 'IK GA AKKOORD'
     },
     pt: {
-        txt1: 'As mulheres nos pediram para não permitir homens que procuram um "relacionamento".  <br><br> Você concorda com este pedido?',
         text1: 'Este NÃO é um site de namoro!',
         text2: 'Você simplesmente escolherá uma mulher e pedirá para transar!',
         text3: 'Por favor, mantenha a identidade delas em segredo!',
         btn: 'EU CONCORDO'
     },
     sl: {
-        txt1: 'Ženske so nas prosile, da ne dovolimo moških, ki iščejo "razmerje".  <br><br> Se strinjate s to zahtevo?',
         text1: 'To NI zmenkarska stran!',
         text2: 'Preprosto izberete žensko in jo vprašate za seks!',
         text3: 'Prosim, ohranite njihovo identiteto skrivnost!',
         btn: 'SE STRINJAM'
     },
     sk: {
-        txt1: 'Ženy nás požiadali, aby sme neumožnili mužom hľadajúcim "vzťah".  <br><br> Súhlasíte s touto žiadosťou?',
         text1: 'Toto NIE JE zoznamka!',
         text2: 'Jednoducho si vyberiete ženu a požiadate ju o sex!',
         text3: 'Prosím, zachovajte ich identitu v tajnosti!',
         btn: 'SÚHLASÍM'
     },
     pl: {
-        txt1: 'Kobiety poprosiły nas, aby nie pozwalać mężczyznom, którzy szukają "związku".  <br><br> Czy zgadzasz się na to żądanie?',
         text1: 'To NIE jest portal randkowy!',
         text2: 'Po prostu wybierzesz kobietę i zapytasz o seks!',
         text3: 'Proszę, zachowaj ich tożsamość w tajemnicy!',
         btn: 'ZGADZAM SIĘ'
     },
     sv: {
-        txt1: 'Kvinnor har bett oss att inte tillåta män som söker ett "förhållande".  <br><br> Håller du med om denna begäran?',
         text1: 'Detta är INTE en dejtingsajt!',
         text2: 'Du väljer helt enkelt en kvinna och frågar om sex!',
         text3: 'Vänligen håll deras identitet hemlig!',
         btn: 'JAG HÅLLER MED'
     },
     no: {
-        txt1: 'Kvinner har bedt oss om å ikke tillate menn som søker et "forhold".  <br><br> Er du enig i denne forespørselen?',
         text1: 'Dette er IKKE en datingside!',
         text2: 'Du velger enkelt en kvinne og spør om sex!',
         text3: 'Vennligst hold deres identitet hemmelig!',
         btn: 'JEG ER ENIG'
     },
     fi: {
-        txt1: 'Naiset ovat pyytäneet meitä olemaan sallimatta miehiä, jotka etsivät "suhdetta".  <br><br> Oletko samaa mieltä tästä pyynnöstä?',
         text1: 'Tämä EI ole deittisivusto!',
         text2: 'Valitset vain naisen ja pyydät seksiä!',
         text3: 'Pidä heidän henkilöllisyytensä salassa!',
         btn: 'OLEN SAMAA MIELTÄ'
     },
     ro: {
-        txt1: 'Femeile ne-au rugat să nu permitem bărbaților care caută o "relație".  <br><br> Sunteți de acord cu această solicitare?',
         text1: 'Acesta NU este un site de întâlniri!',
         text2: 'Pur și simplu vei alege o femeie și vei cere să faceți sex!',
         text3: 'Vă rugăm să păstrați identitatea lor secretă!',
         btn: 'SUNT DE ACORD'
     },
     hu: {
-        txt1: 'A nők azt kérték tőlünk, hogy ne engedjünk olyan férfiakat, akik "kapcsolatot" keresnek.  <br><br> Egyetért ezzel a kéréssel?',
         text1: 'Ez NEM egy társkereső oldal!',
         text2: 'Egyszerűen kiválasztasz egy nőt, és megkéred szexre!',
         text3: 'Kérjük, tartsa titokban a személyazonosságukat!',
